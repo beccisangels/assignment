35cf6075c28fcef79730a2b9de7cdcb3ddc3aae2
rippan <beccisangels@gmail.com>
2022-12-13 23:40:57 +0100

    ta bort rest framework

--- api/views.py
+++ api/views.py
@@ -10,10 +10,14 @@ from .serializers import Message, MessageSerializer, User, UserSerializer
 from django.views import View
 from django.shortcuts import redirect
 from rest_framework.views import APIView
-from rest_framework.decorators import api_view
+
 from django.shortcuts import render
 from django.template import Template, Context
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
+import json
+from django.http import HttpResponse
 
 class MessageList(generics.ListCreateAPIView):
     serializer_class = MessageSerializer
@@ -63,8 +67,10 @@ def delete_albums(request):
 
 
 class Product_View(View):
-
     def get(self, request):
+        print("HEJHEJ")
+        print(self)
+        print(request)
         allproduct = Message.objects.all()
         context = {
             'products':allproduct
@@ -72,14 +78,27 @@ class Product_View(View):
         
         return render(request, "product/index.html", context)
 
+    #@api_view(('POST',))
+    #@renderer_classes((TemplateHTMLRenderer, JSONRenderer))
     def post(self, request, *args, **kwargs):
+        print("TESTPRINT")
         if request.method=="POST":
-            product_ids=request.POST.getlist('id[]')
-            albums = Message.objects.filter(id__in=product_ids)
+            #product_ids=request.POST.getlist('id[]')
+            #product_ids=request.POST.get('id')
+            json_ids = json.loads(request.body)
+
+            """
+            http://127.0.0.1:8000/delete/
+            {
+                "id": [1,2,3]
+            }
+            """
+            albums = Message.objects.filter(id__in=json_ids["id"])
             for album in albums:
                 album.delete()
             serializer = MessageSerializer(albums, many=True)
-            return Response(serializer.data)
+            #return Response(serializer.data)
+            return HttpResponse()
 
 
 class DeleteProducts(View):
